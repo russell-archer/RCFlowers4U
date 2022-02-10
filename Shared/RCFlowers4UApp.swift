b235333b2ae45daddc351c6c5ccac3524f486ba2
Russell Archer <russell.archer@mac.com>
2022-02-10 19:36:42 +0000

    Initial Commit

---
+++ Shared/RCFlowers4UApp.swift
@@ -0,0 +1,17 @@
+//
+//  RCFlowers4UApp.swift
+//  Shared
+//
+//  Created by Russell Archer on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct RCFlowers4UApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
